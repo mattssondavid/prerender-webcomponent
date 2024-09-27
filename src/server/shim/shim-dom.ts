9d4b4305a178c2b2d90cc67c3ad8ad0bd6478463
David Mattsson <mattsson.david@gmail.com>
2024-09-27 21:02:53 +0200

    Improve shimming the DOM by monkey patching ShadowRoot and Element.attachShadow

--- src/server/shim/shim-dom.ts
+++ src/server/shim/shim-dom.ts
@@ -6,8 +6,30 @@ declare global {
         getHTML: (options?: GetHTMLOptions) => string;
     }
 
+    /*
+     * Monkey patch ShadowRoot properties
+     */
     interface SerializableShadowRoot {
-        serializable: boolean;
+        /**
+         * `true` if the shadow root is clonable, and `false` otherwise
+         *
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot/clonable
+         */
+        readonly clonable: boolean;
+
+        /**
+         * `true` if the shadow root delegates focus, and `false` otherwise
+         *
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot/delegatesFocus
+         */
+        readonly delegatesFocus: boolean;
+
+        /**
+         * `true` if the shadow root is serializable, and `false` otherwise
+         *
+         * @see https://developer.mozilla.org/en-US/docs/Web/API/ShadowRoot/serializable
+         */
+        readonly serializable: boolean;
     }
 
     interface Element extends SerializableElement {}
@@ -22,6 +44,9 @@ const patchGlobalThis = (): void => {
     const baseHTML = `<!DOCTYPE html><html><head></head><body></body></html>`;
     const { window } = new JSDOM(baseHTML, { pretendToBeVisual: true });
 
+    /*
+     * Patch/Polyfill `globalThis`
+     */
     const patchedDOMAPIs = [
         'document',
         'customElements',
@@ -33,7 +58,6 @@ const patchGlobalThis = (): void => {
         'Node',
         'ShadowRoot',
     ];
-    // Patch/Polyfill `globalThis`
     patchedDOMAPIs.forEach((domApi: string): void => {
         Reflect.set<typeof globalThis, string>(
             globalThis,
@@ -42,7 +66,50 @@ const patchGlobalThis = (): void => {
         );
     });
 
-    // Polyfill `getHTML`
+    /**
+     * Monkey Patch ShadowRoot.attachShadow from JSDOM while waiting for JSDOM
+     * to fix their ShadowRoot implementation
+     *
+     * @see https://developer.mozilla.org/en-US/docs/Web/API/Element/attachShadow
+     * @see https://github.com/jsdom/jsdom/
+     */
+    const originalAttachShadow = globalThis.Element.prototype.attachShadow;
+    globalThis.Element.prototype.attachShadow = function (
+        options: ShadowRootInit & { clonable?: boolean }
+    ): ShadowRoot {
+        const shadowRoot = originalAttachShadow.apply(this, [options]);
+
+        /*
+         * Set Web API specification defaults and then monkey patch based on the
+         * provided ShadowRoot initialisation options
+         */
+        if (typeof shadowRoot.clonable === 'undefined') {
+            (shadowRoot.clonable as boolean) = false;
+        }
+        if (typeof options.clonable === 'boolean') {
+            (shadowRoot.clonable as boolean) = options.clonable;
+        }
+
+        if (typeof shadowRoot.delegatesFocus === 'undefined') {
+            (shadowRoot.delegatesFocus as boolean) = false;
+        }
+        if (typeof options.delegatesFocus === 'boolean') {
+            (shadowRoot.delegatesFocus as boolean) = options.delegatesFocus;
+        }
+
+        if (typeof shadowRoot.serializable === 'undefined') {
+            (shadowRoot.serializable as boolean) = false;
+        }
+        if (typeof options.serializable === 'boolean') {
+            (shadowRoot.serializable as boolean) = options.serializable;
+        }
+
+        return shadowRoot;
+    };
+
+    /*
+     * Polyfill `getHTML`
+     */
     globalThis.Element.prototype.getHTML ??= function (
         options?: GetHTMLOptions
     ): string {
